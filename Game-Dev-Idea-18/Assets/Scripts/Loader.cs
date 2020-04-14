e995261c6a9c140749634620a98494927957963c
bayanavv <bayanav.sergen@icloud.com>
2020-04-14 19:05:35 +0300

    Update Loader.cs

--- Game-Dev-Idea-18/Assets/Scripts/Loader.cs
+++ Game-Dev-Idea-18/Assets/Scripts/Loader.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Loader : MonoBehaviour
 {
-    
+    private Game m_game = null;
+
+    void Start()
+    {
+        m_game = new Game();
+    }
+
+    void Update()
+    {
+        m_game.Update(Time.deltaTime);
+    }
 }
