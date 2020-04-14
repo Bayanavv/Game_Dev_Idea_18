340a718c5c5127831211e9f21878d958cbe4a7b6
bayanavv <bayanav.sergen@icloud.com>
2020-04-14 14:37:55 +0300

    update

--- Game-Dev-Idea-18/Assets/Scripts/Game.cs
+++ Game-Dev-Idea-18/Assets/Scripts/Game.cs
@@ -4,22 +4,5 @@ using UnityEngine;
 
 public class Game
 {
-    const int AMOUNT = 5; // ?למה רק עם אותיות גדולות
-
-    List<GameObject> m_objs_list = new List<GameObject>();
-    public Game()  // constructor
-    {
-        GameObject go = Resources.Load("Tile") as GameObject;
-        for(int i = 0; i < AMOUNT; i++)
-        {
-            GameObject m_obj = GameObject.Instantiate(go);
-            m_objs_list.Add(m_obj);
-            m_obj.transform.position = new Vector3(0, i * 2.0f, 0);//the position of every object will be 2.0f of ech other
-        }
-    }
-    public void Update(float deltaTime)
-    {
-
-    }
-
+   
 }
--- Game-Dev-Idea-18/Assets/Scripts/Loader.cs
+++ Game-Dev-Idea-18/Assets/Scripts/Loader.cs
@@ -4,20 +4,5 @@ using UnityEngine;
 
 public class Loader : MonoBehaviour
 {
-    private Game m_game = null; //למה כותבים עם אמ
-    private float m_time = 0f;
-    private float m_time_destroy = 0f;
-
-    void Start()
-    {
-        m_game = new Game();
-    }
-
-    void Update()
-    {
-        m_game.Update(Time.deltaTime);
-
-        m_time_destroy += Time.deltaTime;
-        m_time += Time.deltaTime;
-    }
+    
 }
