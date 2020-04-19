81bb74babbbded4603e21b5128e38a04feda7c7f
bayanavv <bayanav.sergen@icloud.com>
2020-04-19 12:56:24 +0300

    Update Game.cs

--- Game-Dev-Idea-18/Assets/Scripts/Game.cs
+++ Game-Dev-Idea-18/Assets/Scripts/Game.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Game
 {
-    const int AMOUNT = 5;
     const int FIGURS = 7;
-    const int TILE_SIZE = 32;
-
+  
     GameObject m_currentFigure = null;
+    Figures m_currentFigureType = Figures.I;
+
     PrefabProvider m_provider = null;
+
+    enum Figures
+    {
+        I = 1,
+        S = 2,
+        S_ = 3,
+        O = 4,
+        T = 5,
+        L = 6,
+        L_ = 7
+    }
 
     public void Update(float deltaTime)
     {
@@ -19,6 +30,7 @@ public class Game
     {
         m_provider = provider;
         CreatRandomFigure();
+        BackGround();
     }
 
     private void CreatRandomFigure()
@@ -28,28 +40,71 @@ public class Game
 
     private void CreateFigure(int randomIndex)
     {
-        switch(randomIndex)
+        m_currentFigureType = (Figures)randomIndex;
+        switch (m_currentFigureType)
         {
-            case 1:
-                CreateTileByArray(new int[,] { { 0, 0, 1, 0 },{ 0, 0, 1, 0 },{ 0, 0, 1, 0 },{ 0, 0, 1, 0 } });
+            case Figures.I:
+                CreateTileByArray(new int[,] {
+                    { 0, 0, 0, 0, 0 },
+                    { 0, 0, 1, 0, 0 },
+                    { 0, 0, 1, 0, 0 },
+                    { 0, 0, 1, 0, 0 },
+                    { 0, 0, 1, 0, 0 }
+                });
                 break;
-            case 2:
-                CreateTileByArray(new int[,] { { 0, 0, 1, 0 },{ 0, 1, 1, 1 },{ 0, 0, 0, 0 },{ 0, 0, 0, 0 } });
+                case Figures.S:
+                CreateTileByArray(new int[,] {
+                    { 0, 0, 0, 0, 0 },
+                    { 0, 1, 1, 0, 0 },
+                    { 0, 0, 1, 1, 0 },
+                    { 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0 }
+                });
                 break;
-            case 3:
-                CreateTileByArray(new int[,] { { 0, 0, 0, 0 }, { 0, 1, 1, 0 },{ 0, 0, 1, 1 },{ 0, 0, 0, 0 } });
+                case Figures.S_:
+                CreateTileByArray(new int[,] {
+                    { 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0 },
+                    { 0, 0, 1, 1, 0 },
+                    { 0, 1, 1, 0, 0 },
+                    { 0, 0, 0, 0, 0 }
+                });
                 break;
-            case 4:
-                CreateTileByArray(new int[,] { { 0, 0, 0, 0 }, { 0, 0, 1, 1 }, { 0, 1, 1, 0 },{ 0, 0, 0, 0 } });
+            case Figures.O:
+                CreateTileByArray(new int[,] {
+                    { 0, 0, 0, 0, 0 },
+                    { 0, 1, 1, 0, 0 },
+                    { 0, 1, 1, 0, 0 },
+                    { 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0 }
+                });
                 break;
-            case 5:
-                CreateTileByArray(new int[,] { { 0, 0, 1, 0 }, { 0, 0, 1, 0 },{ 0, 1, 1, 0 },{ 0, 0, 0, 0 } });
+            case Figures.T:
+                CreateTileByArray(new int[,] {
+                    { 0, 0, 0, 0, 0 },
+                    { 0, 0, 1, 0, 0 },
+                    { 0, 1, 1, 1, 0 },
+                    { 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0 }
+                });
                 break;
-            case 6:
-                CreateTileByArray(new int[,] { { 0, 0, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 1, 1 }, { 0, 0, 0, 0 } });
+            case Figures.L:
+                CreateTileByArray(new int[,] {
+                    { 0, 0, 1, 0, 0 },
+                    { 0, 0, 1, 0, 0 },
+                    { 0, 0, 1, 1, 0 },
+                    { 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0 }
+                });
                 break;
-            case 7:
-                CreateTileByArray(new int[,] { { 0, 0, 0, 0 }, { 0, 1, 1, 0 }, { 0, 1, 1, 0 }, { 0, 0, 0, 0 } });
+            case Figures.L_:
+                CreateTileByArray(new int[,] {
+                    { 0, 0, 1, 0, 0 },
+                    { 0, 0, 1, 0, 0 },
+                    { 0, 1, 1, 0, 0 },
+                    { 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0 }
+                });
                 break;
         }
 
@@ -57,26 +112,64 @@ public class Game
 
     private void CreateTileByArray(int[,] array)
     {
-        for (int i = 0; i < 4; i++)
+        m_currentFigure = GameObject.Instantiate(m_provider.FigurBase);
+        for (int i = 0; i < 5; i++)
         {
-            for (int j = 0; j < 4; j++)
+            for (int j = 0; j < 5; j++)
             { 
                 if(array[i, j] == 1)
                 {
-                    CreateTile(i,j);
+                    CreateTile(i, j, m_currentFigure.transform);
                 }
             }
         }
     }
 
-    private void CreateTile(int i, int j)
+    private void CreateTile(int i, int j, Transform parentTransform)
     {
-        GameObject parent = new GameObject();
-        //parent.AddComponent<Transform>();
-        m_currentFigure = GameObject.Instantiate(parent, m_provider.FigureStartPoint.transform);//put the figur in spicific point
-
         GameObject go = Resources.Load("Tile") as GameObject;
-        GameObject m_obj = GameObject.Instantiate(go, m_currentFigure.transform);
-        go.transform.position = new Vector3(i * TILE_SIZE - 1.5f * TILE_SIZE, j * TILE_SIZE - 1.5f * TILE_SIZE, 0);
+        SpriteRenderer renderer = go.GetComponent<SpriteRenderer>();
+
+        float size = 0f;
+
+        if (renderer != null)
+        {
+            size = renderer.size.x;
+        }
+
+        GameObject obj = GameObject.Instantiate(go);
+
+        float x = j * size - 2f * size;
+        float y = i * size - 2f * size;
+        float z = 0;
+
+        obj.transform.position = new Vector3(x, y, z);
+        obj.transform.parent = parentTransform;
+    }
+
+    public void RotateFigut()
+    {
+        switch (m_currentFigureType)
+        {
+            case Figures.I:
+                if (m_currentFigure.transform.eulerAngles.z == 0)
+                {
+                    m_currentFigure.transform.Rotate(new Vector3(0, 0, 90f));
+                }
+                else
+                {
+                    m_currentFigure.transform.Rotate(new Vector3(0, 0, -90f));
+                }
+                break;
+            case Figures.T:
+                m_currentFigure.transform.Rotate(new Vector3(0, 0, 90f));
+                break;
+        }
+    }
+
+    public void BackGround()
+    {
+        GameObject backGround_go = Resources.Load("BackGroundTile") as GameObject;
+        GameObject backGround_obj = GameObject.Instantiate(backGround_go);
     }
 }
